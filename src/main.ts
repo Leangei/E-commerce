7b8a4093a04df494a59dfbc50e67f7f463ce7643
Leangei <leangeimeng1602@gmail.com>
2025-11-29 10:18:55 +0700

    TP04-Router

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-const pinia = createPinia()  
+const pinia = createPinia()
 
-app.use(pinia)               
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '../views/HomeView.vue'
+import CategoryView from '../views/CategoryView.vue'
+import ProductView from '../views/ProductView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path:"/",
+      name: "home",
+      component: () => import( '../views/HomeView.vue'),
+    },
+    {
+      path:"/categories/:categoryId",
+      name: "category",
+      component: () => import( '../views/CategoryView.vue'),
+    },
+    {
+      path:"/products/:productId",
+      name: "product",
+      component: () => import( '../views/ProductView.vue'),
+
+    },
+  ],
+});
 
 export default router
